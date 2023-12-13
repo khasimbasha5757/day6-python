18fe116ca9d17d50ae2cb91771c415633db6c047
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2023-12-13 15:50:58 +0530

    input in python

---
+++ 6th day.py
@@ -0,0 +1,12 @@
+# user input
+# print("tell ur name : ")
+# a=input("Tell ur name: ")
+# print("Ur name u have given is :",a)
+#it gives us the value as an integer
+a=int(input("Value of x is : "))
+b=int(input("Value of y is :"))
+print("Value of a+b is ", a+b )
+#it gives us the value in terms as strings
+a=input("Value of x is : ")
+b=input("Value of y is :")
+print("Value of a+b is ", int(a)+int(b) )
